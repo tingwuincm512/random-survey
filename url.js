0f5506f2403ff9b880bcc1181b97c233b9dba62e
tingwuincm512 <150567845+tingwuincm512@users.noreply.github.com>
2025-01-24 16:57:46 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,10 @@
 
 
 const urls = [
+	'https://www.surveycake.com/s/PgxB2',
+	'https://www.surveycake.com/s/2MQn3',
+	'https://www.surveycake.com/s/mV18n',
 	'https://www.surveycake.com/s/wrGMP',
-	'https://www.surveycake.com/s/zx3OL',
-	'https://www.surveycake.com/s/3lZeR',
+	'https://www.surveycake.com/s/VwboR',
+	'https://www.surveycake.com/s/zxX3w',
 ];
